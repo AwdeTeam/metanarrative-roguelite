f5145392c1d6a0f6c9eea154e6d4f455cca4524e
Martindale, Nathan <nathanamartindale@gmail.com>
2023-01-14 16:28:57 -0500

    Start creating class structure

---
+++ Entity.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+export (int) var strength = 0
+export (int) var health = 0
+export (int) var pride = 0
+export (int) var protection = 0
+
+# ??
+#export (int) var renown = 0
+#export (int) var sanity = 0
---
+++ Storyteller.gd
@@ -0,0 +1,30 @@
+extends Node
+
+# https://docs.godotengine.org/en/stable/tutorials/scripting/singletons_autoload.html#doc-singletons-autoload
+
+
+export (int) var health = 1
+export (Array) var characters = []
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func do_thing():
+	pass
+
+
+func generate_cities():
+	pass
