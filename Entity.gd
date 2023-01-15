809c204efb0a7517148763ebfb5f1f2e9a166b8f
Martindale, Nathan <nathanamartindale@gmail.com>
2023-01-14 20:05:53 -0500

    Add basic story startup code

--- Entity.gd
+++ Entity.gd
@@ -1,11 +1,25 @@
 extends Node
 
 
-export (int) var strength = 0
-export (int) var health = 0
-export (int) var pride = 0
-export (int) var protection = 0
+export (String) var entity_name = ""
+export (String) var type = ""  # "player" or "city"
+export (float) var strength = 0.0
+export (float) var health = 0.0
+export (float) var pride = 0.0
+export (float) var protection = 0.0
+
+# unclear if these are really necessary, as should be by-product of above 4
+export (float) var alignment_ge = 0.0  # good-evil
+export (float) var alignment_lc = 0.0  # lawful-chaotic
+
+export (Dictionary) var arc_weights = {"redemption": 0.0, "sacrifice": 0.0, "revenge": 0.0}
+export (Array) var story_path = []
+export (Dictionary) var story_graph = {}
 
 # ??
 #export (int) var renown = 0
 #export (int) var sanity = 0
+
+# func _init():
+#     self.entity_name = entity_name
+#     pass
--- Storyteller.gd
+++ Storyteller.gd
@@ -1,11 +1,13 @@
+# https://docs.godotengine.org/en/stable/tutorials/scripting/singletons_autoload.html#doc-singletons-autoload
 extends Node
 
-# https://docs.godotengine.org/en/stable/tutorials/scripting/singletons_autoload.html#doc-singletons-autoload
+const Entity = preload("res://Entity.gd")
 
 
-export (int) var health = 1
+
 export (Array) var characters = []
-
+export (Array) var cities = []
+export (String) var main_arc = ""
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -22,9 +24,51 @@ func _ready():
 #	pass
 
 
-func do_thing():
-	pass
+func begin_story():
+	self.generate_cities()
+	self.generate_entities()
+	self.generate_initial_story()
 
 
 func generate_cities():
-	pass
+	for i in 5:
+		var city = Entity.new()
+		city.type = "city"
+		city.entity_name = "Beantown " + String(i)
+		cities.append(city)
+
+
+func generate_entities():
+	# generate protagonist 
+	var pro = Entity.new()
+	pro.type = "player"
+	pro.entity_name = "Mr. Bean"
+	characters.append(pro)
+	
+	# generate antagonist
+	var ant = Entity.new()
+	ant.type = "player"
+	ant.entity_name = "Alejandro Garbanzo"
+	characters.append(ant)
+	
+	# generate companion
+	var comp = Entity.new()
+	comp.type = "player"
+	comp.entity_name = "Linetti Cannellini"
+	characters.append(comp)
+
+
+func generate_initial_story():
+	# choose arc
+	var arc_choice = randi() % 3
+
+	if arc_choice == 0:
+		main_arc = "redemption"
+	elif arc_choice == 1:
+		main_arc = "sacrifice"
+	elif arc_choice == 2:
+		main_arc = "revenge"
+	
+	# choose initial character to apply to and softly put weight there
+	var player_choice = randi() % 3
+	characters[player_choice].arc_weights[main_arc] = 1.0
